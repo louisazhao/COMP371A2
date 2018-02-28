0ec2b7220843a55bc0608a3cb13060d6c398d974
louisazhao <32183353+louisazhao@users.noreply.github.com>
2018-02-28 00:02:59 -0500

    square

--- squarefs.fs
+++ squarefs.fs
@@ -1,20 +1,46 @@
 #version 330 core
 in vec2 texCoordinate;
+in vec4 FragPosLightSpace;
+in vec3 Normals;
+in vec3 FragPos;
+in vec3 LightPos;
 
 out vec4 FragColor;
 
 uniform bool texOn;
 uniform sampler2D grassTex;
+uniform sampler2D shadowMap;
+uniform vec3 lightColor;
 
 void main()
 {
+    //ambient
+    float ambientDegree=0.5;
+    vec3 ambient=ambientDegree*lightColor;
+    
+    //diffuse
+    vec3 norm=normalize(Normals);
+    vec3 lightDir=normalize(LightPos-FragPos);
+    float diff=max(dot(norm,lightDir),0.0f);
+    vec3 diffuse=diff*lightColor;
+    
+    //specular
+    float specularStrength=0.5f;
+    vec3 viewDir=normalize(-FragPos);
+    vec3 reflectDir=reflect(-lightDir,norm);
+    float spec=pow(max(dot(viewDir,reflectDir),0.0),32);
+    vec3 specular=specularStrength*spec*lightColor;
+    
     if(texOn==false)
     {
-        FragColor=vec4(0.5f,0.5f,0.5f,1.0f);
+        vec4 color=vec4(0.5f,0.5f,0.5f,1.0f);
+        vec4 result=vec4((ambient+diffuse+specular),1.0f)*color;
+        FragColor = result;
     }
     else
     {
-        FragColor=texture(grassTex,texCoordinate);
+        vec3 lightResult=(ambient+diffuse+specular);
+        FragColor=vec4(lightResult,1.0)*texture(grassTex,texCoordinate);
     }
 }
 
--- squarevs.vs
+++ squarevs.vs
@@ -1,15 +1,28 @@
 #version 330 core
 layout(location=0) in vec3 aPos;
-layout(location=1) in vec2 aTexCoord;
+layout(location=1) in vec3 normals;
+layout(location=2) in vec2 aTexCoord;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform mat4 lightSpaceMatrix;
+uniform vec3 lightPos;
+
+
 out vec2 texCoordinate;
+out vec4 FragPosLightSpace;
+out vec3 Normals;
+out vec3 FragPos;
+out vec3 LightPos;
 
 void main()
 {
     gl_Position=projection*view*model*vec4(aPos,1.0);
+    FragPos=vec3(view*model * vec4(aPos, 1.0));
+    Normals=mat3(transpose(inverse(view*model)))*normals;
+    LightPos=vec3(view*vec4(lightPos,1.0f));
     texCoordinate=vec2(aTexCoord.x,aTexCoord.y);
+    FragPosLightSpace = lightSpaceMatrix * vec4(FragPos, 1.0);
 }
 
